f45b7f214d64ffe74be2a3b53d828eff3deb3ff2
nishant51 <timsinanishant51@gmail.com>
2026-01-22 14:49:07 +0545

    fix:commented out the celery and reddis

--- let_me_learn_eng/celery.py
+++ let_me_learn_eng/celery.py
@@ -1,12 +1,12 @@
-import os
-from celery import Celery
+# import os
+# from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "let_me_learn_eng.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "let_me_learn_eng.settings")
 
-app = Celery("let_me_learn_eng")
+# app = Celery("let_me_learn_eng")
 
-# Read config from Django settings, using CELERY_ prefix
-app.config_from_object("django.conf:settings", namespace="CELERY")
+# # Read config from Django settings, using CELERY_ prefix
+# app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Auto-discover tasks.py in installed apps
-app.autodiscover_tasks()
+# # Auto-discover tasks.py in installed apps
+# app.autodiscover_tasks()
