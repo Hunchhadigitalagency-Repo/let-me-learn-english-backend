f21fe2f8c6d3a806cc6e3eeb7ed3bd9030ff0ae3
nishant51 <timsinanishant51@gmail.com>
2026-01-26 14:43:37 +0545

    fix: added the table model

--- tasks/serializers/nested_tasks_Serializers.py
+++ tasks/serializers/nested_tasks_Serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import (
+from tasks.models import (
     Task,
     SpeakingActivity, SpeakingActivityQuestion, speakingActivitySample,
     ReadingActivity, ReadingAcitivityQuestion,
