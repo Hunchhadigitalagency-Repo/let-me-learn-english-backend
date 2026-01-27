8ec18df7a87400b08c6c8687a55a1455d2c7fbb4
nishant51 <timsinanishant51@gmail.com>
2026-01-27 16:30:40 +0545

    fix

--- let_me_learn_eng/settings.py
+++ let_me_learn_eng/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-dev-key")  
-DEBUG = os.getenv("DEBUG", "True") == "True"           
-
-# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = os.getenv("DEBUG", "True") == "True"           
 DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production!
 
 # settings.py
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
